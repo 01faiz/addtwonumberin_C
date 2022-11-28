b654ea3faa4e8d261bb88e214dab332f69bc2711
Alfaiz <102668382+01faiz@users.noreply.github.com>
2022-11-28 20:18:55 +0530

    Add files via upload

---
+++ addtwonumber.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    int A,B,sum=0;
+
+    printf("ENTER THE A AND B VALUE : \n");
+    scanf("%d %d",&A,&B);
+
+    sum= A+B;
+
+    printf("SUM OF A AND  B IS : %d ", sum);
+
+    return 0;
+    
+}
